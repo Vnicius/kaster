35f19c62ec316d28fca36ecc97643a760d3bbaa9
Vinicius <vinicius.matheus252@gmail.com>
2018-01-16 23:03:11 -0300

    Favorites screen

---
+++ kaster/src/screens/FavoritesScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { 
+  Text,
+} from 'react-native';
+
+export default class FavoritesScreen extends Component {
+  render() {
+    return (
+      <Text>FavoritesScreen</Text>
+    )
+  }
+};
