acd8124cc8681fb28869671325d68ecd56e4b9d8
Vinicius <vinicius.matheus252@gmail.com>
2018-01-16 23:03:46 -0300

    Releases screen

---
+++ kaster/src/screens/ReleasesScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { 
+  Text,
+} from 'react-native';
+
+export default class ReleasesScreen extends Component {
+  render() {
+    return (
+      <Text>ReleasesScreen</Text>
+    )
+  }
+};
