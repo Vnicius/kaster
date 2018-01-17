8ea27ad2df12d3dde4b19acb98a6795d4de8f791
Vinicius <vinicius.matheus252@gmail.com>
2018-01-17 13:20:15 -0300

    Adaptive toolbar

--- kaster/src/components/Toolbar.js
+++ kaster/src/components/Toolbar.js
@@ -2,16 +2,24 @@ import React, { Component } from 'react';
 import { 
 	ToolbarAndroid,
 	StyleSheet,
+	View,
 } from 'react-native';
 
 export default class Toolbar extends Component {
   render() {
     return (
       <ToolbarAndroid
+				title={this.props.title ? this.props.title : ""}
 				style={styles.toolBar}
 				titleColor="white"
-				title={this.props.title}
-    	/>
+    	>
+				<View style={{
+					height: 56,
+					backgroundColor: "#512da8",
+				}}>
+				{this.props.content}
+				</View>
+			</ToolbarAndroid>
     )
   }
 };
