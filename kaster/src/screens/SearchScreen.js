2a652242665041a840d2313933c2e65b5800a8b6
Vinicius <vinicius.matheus252@gmail.com>
2018-01-17 13:20:48 -0300

    Search screen with toobar search

--- kaster/src/screens/SearchScreen.js
+++ kaster/src/screens/SearchScreen.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import { 
   Text,
   View,
+  TextInput,
+
 } from 'react-native';
 
+import Toolbar from '../components/Toolbar';
+import ToolbarSearch from '../components/ToolbarSearch';
+
 export default class SearchScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Useless Placeholder' };
+  }
   render() {
     return (
       <View>
+        <Toolbar content={<ToolbarSearch />}/>
         <Text>SearchScreen</Text>
       </View>
     )
