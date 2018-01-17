e358f71b9eda38ef64938a9db5137783503d469c
Vinicius <vinicius.matheus252@gmail.com>
2018-01-16 23:03:18 -0300

    Listening screen

---
+++ kaster/src/screens/ListeningScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { 
+  Text,
+} from 'react-native';
+
+export default class ListeningScreen extends Component {
+  render() {
+    return (
+      <Text>ListeningScreen</Text>
+    )
+  }
+};
