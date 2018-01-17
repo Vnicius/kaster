015952872ddbbe38f0000b1a900583e510aae647
Vinicius <vinicius.matheus252@gmail.com>
2018-01-16 23:04:14 -0300

    Susbscriptions screen

---
+++ kaster/src/screens/SubscriptionsScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { 
+  Text,
+} from 'react-native';
+
+export default class SubscriptionsScreen extends Component {
+  render() {
+    return (
+      <Text>SubscriptionsScreen</Text>
+    )
+  }
+};
