dd6aa4e25ead8bc2a2b79951424105ab9d066da5
Vinicius <vinicius.matheus252@gmail.com>
2018-01-22 20:48:57 -0300

    Podcast Detail Screen

---
+++ kaster/src/screens/PodcastDetailScreen.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  ScrollView,
+} from 'react-native';
+
+export default class PodcastDetailScreen extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.state.params.name,
+  });
+
+  render() {
+		const { params } = this.props.navigation.state;
+    return (
+      <ScrollView style={{elevation: 10}}>
+        <Text>{params.description}</Text>
+      </ScrollView>
+    )
+  }
+};
