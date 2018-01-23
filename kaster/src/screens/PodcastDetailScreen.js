deee32ab9801d696d1edb7e449f7a294383dce59
Vinicius <vinicius.matheus252@gmail.com>
2018-01-22 21:04:58 -0300

    Detail Screen without nav bar

--- kaster/src/screens/PodcastDetailScreen.js
+++ kaster/src/screens/PodcastDetailScreen.js
@@ -7,6 +7,7 @@ import {
 export default class PodcastDetailScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: navigation.state.params.name,
+    tabBarVisible: false,
   });
 
   render() {
