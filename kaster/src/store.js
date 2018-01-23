8eca57a259566461a2f59d2cc1b7a4d649debaba
Vinicius <vinicius.matheus252@gmail.com>
2018-01-23 17:34:07 -0300

    Add searchReducer

--- kaster/src/store.js
+++ kaster/src/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise-middleware';
+import logger from 'redux-logger'
 import allReducers from './reducers';
 
-const middlewares = applyMiddleware(promise());
+const middlewares = applyMiddleware(promise(), logger);
 export default createStore(allReducers, middlewares);
