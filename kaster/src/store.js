9502f1fab4d60f35e38f7be64800c16fbbde2d0e
Vinicius <vinicius.matheus252@gmail.com>
2018-01-24 20:08:23 -0300

    Add thunk middleware

--- kaster/src/store.js
+++ kaster/src/store.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise-middleware';
 import logger from 'redux-logger'
+import thunk from 'redux-thunk';
 import allReducers from './reducers';
 
-const middlewares = applyMiddleware(promise(), logger);
+const middlewares = applyMiddleware(promise(), logger, thunk);
 export default createStore(allReducers, middlewares);
