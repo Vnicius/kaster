1627caa5bcea00cf92c699155419cce0541f0d77
Vinicius <vinicius.matheus252@gmail.com>
2018-02-07 23:21:39 -0300

    Podcasts Screen showing the signed podcasts

--- kaster/src/screens/PodcastsScreen.js
+++ kaster/src/screens/PodcastsScreen.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 import { 
   Text,
+  View,
 } from 'react-native';
 
-export default class PodcastsScreen extends Component {
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { getSignedPodcasts } from '../actions/podcastAction';
+import SquarePodcastsContainer from '../components/SquarePodcastsContainer';
+
+class PodcastsScreen extends Component {
   render() {
+    if(!this.props.signedPodcasts) {
+      this.props.getSignedPodcasts();
+    }
     return (
-      <Text>PodcastsScreen</Text>
+      <View>
+        {
+          this.props.signedPodcasts
+          ? <SquarePodcastsContainer podcasts={this.props.signedPodcasts}
+                                     onPress={() => alert("Role")}/>
+          : <Text>No Podcasts!</Text>
+        }
+      </View>
     )
   }
 };
+
+function mapStateToProps(state) {
+  return {
+    signedPodcasts: state.podcast.signedPodcasts,
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    getSignedPodcasts: getSignedPodcasts,
+  },dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PodcastsScreen);
